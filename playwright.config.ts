97938e1c64ed4ebefcaa3c2dbdd477e076add63d
TaroTest123 <trailhead.practice@gmail.com>
2024-06-23 10:28:58 +0000

    qase test

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,17 +1,13 @@
 import { defineConfig, devices } from '@playwright/test';
-
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import path from 'path';
+import dotenv from 'dotenv';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests-examples',
+  testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -21,7 +17,29 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['list'],
+    ['html', { open: 'never' }], // Add the HTML reporter
+    [
+      'playwright-qase-reporter',
+      {
+        debug: true,
+
+        testops: {
+          api: {
+            token: process.env.QASE_API_KEY,
+          },
+
+          project: process.env.QASE_PROJECT_CODE,
+          uploadAttachments: true,
+
+          run: {
+            complete: true,
+          },
+        },
+      },
+    ],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
---
+++ tests/example-qase.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test';
+import { qase } from 'playwright-qase-reporter';
+
+test.describe('Test suite', () => {
+
+  test('has title', async ({ page }) => {
+    qase.id(14);
+    qase.title('has title');
+  
+    await page.goto('https://playwright.dev/');
+  
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Playwright/);
+  });
+
+  test('get started link', async ({ page }) => {
+    qase.id(15);
+    qase.title('get started link');
+
+    await page.goto('https://playwright.dev/');
+  
+    // Click the get started link.
+    await page.getByRole('link', { name: 'Get started' }).click();
+  
+    // Expects page to have a heading with the name of Installation.
+    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  });
+
+});
